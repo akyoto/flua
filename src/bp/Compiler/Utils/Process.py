372acc636b90a482050aea75a9cb661df5aaf737
Eduard Urbach <e.urbach@gmail.com>
2012-05-10 14:16:55 +0200

    Improved console output

--- src/bp/Compiler/Utils/Process.py
+++ src/bp/Compiler/Utils/Process.py
@@ -5,6 +5,7 @@ def startProcess(cmd, fhOut, fhErr):
 	line = ""
 	errLine = ""
 	proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	linesThreshold = 2000
 	
 	while 1:
 		# Make the GUI feel more responsive
@@ -12,11 +13,21 @@ def startProcess(cmd, fhOut, fhErr):
 		
 		combinedLinesStdout = []
 		combinedLinesStderr = []
+		combinedLinesStdoutCount = 0
+		combinedLinesStderrCount = 0
 		
 		while 1:
 			line = proc.stdout.readline()
 			if line:
 				combinedLinesStdout.append(line.decode("utf-8"))
+				combinedLinesStdoutCount += 1
+				
+				# For programs which produce too much output at once
+				if combinedLinesStdoutCount > linesThreshold:
+					fhOut(''.join(combinedLinesStdout))
+					combinedLinesStdout = []
+					combinedLinesStdoutCount = 0
+					QtGui.QApplication.instance().processEvents()
 			else:
 				break
 			
@@ -24,12 +35,20 @@ def startProcess(cmd, fhOut, fhErr):
 			errLine = proc.stderr.readline()
 			if errLine:
 				combinedLinesStderr.append(errLine.decode("utf-8"))
+				combinedLinesStderrCount += 1
+				
+				# For programs which produce too much output at once
+				if combinedLinesStderrCount > linesThreshold:
+					fhErr(''.join(combinedLinesStderr))
+					combinedLinesStderr = []
+					combinedLinesStderrCount = 0
+					QtGui.QApplication.instance().processEvents()
 			else:
 				break
 		
 		exitCode = proc.poll()
 			
-		if (not combinedLinesStdout) and (not combinedLinesStderr) and (exitCode is not None):
+		if (not combinedLinesStdoutCount) and (not combinedLinesStderrCount) and (exitCode is not None):
 			break
 		
 		if combinedLinesStdout:
