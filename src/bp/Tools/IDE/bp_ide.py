487383194d7474b3ccd6c4715ede0fe91cb4c7a9
Eduard Urbach <e.urbach@gmail.com>
2012-05-11 18:23:16 +0200

    DFC

--- src/bp/Tools/IDE/bp_ide.py
+++ src/bp/Tools/IDE/bp_ide.py
@@ -46,7 +46,7 @@ class BPMainWindow(QtGui.QMainWindow, MenuActions, Startup, Benchmarkable):
 		print("Module directory: " + getModuleDir())
 		print("---")
 		
-		self.developerFlag = 1#False
+		self.developerFlag = False
 		self.tmpCount = 0
 		self.lastBlockPos = -1
 		self.lastFunctionCount = -1
