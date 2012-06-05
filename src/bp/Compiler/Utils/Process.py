4b7256be1ad64ea39b0ca533161c06df740230d4
Eduard Urbach <e.urbach@gmail.com>
2012-06-05 22:07:48 +0200

    Replaced console log method with the old one due to performance reasons

--- src/bp/Compiler/Utils/Process.py
+++ src/bp/Compiler/Utils/Process.py
@@ -47,13 +47,75 @@ def startProcess(cmd, fhOut, fhErr, thread = None):
 		stderr = subprocess.PIPE,
 		stdin = subprocess.PIPE,
 		env = envi,
-		bufsize = 1,
+		#bufsize = 1,
 		close_fds = ON_POSIX
 	)
 	
 	if thread:
 		thread.process = proc
+		
+	linesThreshold = 2000
+	timeThreshold = 1.0 / 25 # seconds
+	lastWriteTime = 0
+	lastErrWriteTime = 0
 	
+	while 1:
+		# Make the GUI feel more responsive
+		QtGui.QApplication.instance().processEvents()
+
+		combinedLinesStdout = []
+		combinedLinesStderr = []
+		combinedLinesStdoutCount = 0
+		combinedLinesStderrCount = 0
+
+		while 1:
+			line = proc.stdout.readline()
+			if line:
+				combinedLinesStdout.append(line.decode("utf-8"))
+				combinedLinesStdoutCount += 1
+
+				# For programs which produce too much output at once
+				if combinedLinesStdoutCount > linesThreshold or time.time() - lastWriteTime > timeThreshold:
+					fhOut(''.join(combinedLinesStdout))
+					combinedLinesStdout = []
+					combinedLinesStdoutCount = 0
+					QtGui.QApplication.instance().processEvents()
+					lastWriteTime = time.time()
+			else:
+				break
+
+		while 1:
+			errLine = proc.stderr.readline()
+			if errLine:
+				combinedLinesStderr.append(errLine.decode("utf-8"))
+				combinedLinesStderrCount += 1
+
+				# For programs which produce too much output at once
+				if combinedLinesStderrCount > linesThreshold or time.time() - lastErrWriteTime > timeThreshold:
+					fhErr(''.join(combinedLinesStderr))
+					combinedLinesStderr = []
+					combinedLinesStderrCount = 0
+					QtGui.QApplication.instance().processEvents()
+					lastErrWriteTime = time.time()
+			else:
+				break
+
+		exitCode = proc.poll()
+		QtGui.QApplication.instance().processEvents()
+
+		if (not combinedLinesStdoutCount) and (not combinedLinesStderrCount) and (exitCode is not None):
+			return exitCode
+
+		if combinedLinesStdout:
+			fhOut(''.join(combinedLinesStdout))
+
+		if combinedLinesStderr:
+			fhErr(''.join(combinedLinesStderr))
+
+		lastWriteTime = time.time()
+		lastErrWriteTime = time.time()
+	
+def disabled():
 	q = Queue()
 	errQ = Queue()
 	
@@ -65,7 +127,7 @@ def startProcess(cmd, fhOut, fhErr, thread = None):
 	tErr.daemon = True # thread dies with the program
 	tErr.start()
 	
-	writeEveryXSecs = 1.0 / 25
+	writeEveryXSecs = 1.0 / 10
 	lastWrite = time.time()
 	
 	chars = []
