27bf661054fd72537677772daeec5034d5578e15
Eduard Urbach <e.urbach@gmail.com>
2012-06-07 19:38:02 +0200

    Testing Irrlicht usage

--- src/bp/Graphics/Irrlicht.hpp
+++ src/bp/Graphics/Irrlicht.hpp
@@ -1,7 +1,6 @@
 #include <irrlicht/irrlicht.h>
 
 using namespace irr;
-
 using namespace core;
 using namespace scene;
 using namespace video;
@@ -10,31 +9,109 @@ using namespace gui;
 
 #pragma comment(lib, "Irrlicht.lib")
 
-inline void bp_irrHelloWorld() {
-	IrrlichtDevice *device =
-		createDevice(EDT_SOFTWARE, dimension2d<u32>(512, 384), 16,
-			false, false, false, 0);
+// Typedefs
+#define BPIrrlichtDevice IrrlichtDevice
+
+// Global
+IrrlichtDevice *irrDevice;
+IVideoDriver* irrDriver;
+ISceneManager* irrSmgr;
+IGUIEnvironment* irrGuienv;
+
+inline void bp_cls() {
+	irrDriver->beginScene(true, true, SColor(255, 0, 0, 0));
+}
+
+inline void bp_flip() {
+	irrSmgr->drawAll();
+	irrGuienv->drawAll();
 	
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+	irrDriver->endScene();
+}
+
+inline void bp_closeGraphicsWindow() {
+	irrDevice->drop();
+}
+
+inline IrrlichtDevice* bp_createIrrDevice(char* title, int width, int height, int depth, bool fullscreen) {
+	irrDevice = createDevice(EDT_OPENGL, dimension2d<u32>(width, height), depth, fullscreen, false, false, 0);
 	
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
+	stringw titleWstr(title);
+	irrDevice->setWindowCaption(titleWstr.c_str());
 	
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software engine!",
-	 rect<int>(10,10,200,22), true);
+	irrDriver = irrDevice->getVideoDriver();
+	irrSmgr = irrDevice->getSceneManager();
+	irrGuienv = irrDevice->getGUIEnvironment();
 	
-	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+	//irrGuienv->addStaticText(L"OpenGL", rect<int>(10,10,200,22), true);
 	
-	while(device->run())
+	irrSmgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+	
+	return irrDevice;
+	
+	/*while(bp_appRunning())
 	{
-		driver->beginScene(true, true, SColor(255,100,101,140));
-	
-		smgr->drawAll();
-		guienv->drawAll();
-	
-		driver->endScene();
+		bp_cls();
+		
+		bp_flip();
 	}
 	
-	device->drop();
+	bp_closeGraphicsWindow();*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
