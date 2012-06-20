55a45f81ca9840eeae585e5a0fe60017b4d993e9
Eduard Urbach <e.urbach@gmail.com>
2012-06-20 16:39:52 +0200

    Implemented a few more string functions

--- src/bp/Core/String/CharTypes.hpp
+++ src/bp/Core/String/CharTypes.hpp
@@ -4,4 +4,5 @@ BP_WRAP_1(bool, bp_isAlnum, isalnum)
 BP_WRAP_1(bool, bp_isAlpha, isalpha)
 BP_WRAP_1(bool, bp_isDigit, isdigit)
 BP_WRAP_1(bool, bp_isSpace, isspace)
-
+BP_WRAP_1(bool, bp_isUpper, isupper)
+BP_WRAP_1(bool, bp_isLower, islower)
