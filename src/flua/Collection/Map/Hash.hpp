8ba7a599d686161364055b8ca00f0e958649dff7
Eduard Urbach <e.urbach@gmail.com>
2012-07-05 00:17:40 +0200

    Heavily improved hash algorithm

--- src/flua/Collection/Map/Hash.hpp
+++ src/flua/Collection/Map/Hash.hpp
@@ -1,16 +1,45 @@
 // C/C++ files
-#include <hash/phsieh.hpp>
+//#include <hash/phsieh.hpp>
+#include <hash/murmur3.cpp>
 
 // bp modules
 #include <flua/Core/String/UTF8String/C++/UTF8String.hpp>
-#define HPT(x) reinterpret_cast<const char*>(x)
+
+// Hash Pointer Type
+//#define HPT(x) reinterpret_cast<const char*>(x)
+#define HPT(x) reinterpret_cast<void*>(x)
+#define MURMUR_SEED 32
 
 // Hash functions
 
+/* Murmur3 */
+template <typename T>
+inline Int32 flua_hash(T key) {
+	Int32 h;
+	MurmurHash3_x86_32(HPT(&key), sizeof(T), MURMUR_SEED, &h);
+	return h;
+}
+
+// Strings
+inline Int32 flua_hash(BPUTF8String* key) {
+	Int32 h;
+	MurmurHash3_x86_32(HPT(key->_data), key->_length, MURMUR_SEED, &h);
+	return h;
+}
+
+// Other objects
+template <typename T>
+inline Int32 flua_hash(T* key) {
+	Int32 h;
+	MurmurHash3_x86_32(HPT(&key), sizeof(T*), MURMUR_SEED, &h);
+	return h;
+}
+
+/* P. Hsieh
 // Int, Float, etc.
 template <typename T>
 inline Int32 flua_hash(T key) {
-	return SuperFastHash(HPT(&key), sizeof(T));
+	return MurmurHash3_x86_32(HPT(&key), sizeof(T));
 }
 
 // Strings
@@ -22,7 +51,7 @@ inline Int32 flua_hash(BPUTF8String* key) {
 template <typename T>
 inline Int32 flua_hash(T* key) {
 	return SuperFastHash(HPT(&key), sizeof(T*));
-}
+}*/
 
 /*
 // This function can hash the content of an object instead of its pointer value
