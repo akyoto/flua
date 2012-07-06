75aa26f08416468605d0abb7e570f7cffb5dd29e
Eduard Urbach <e.urbach@gmail.com>
2012-07-06 13:31:02 +0200

    Improved documentation navigator

--- src/flua/Tools/IDE/Widgets/BPDocNavigator.py
+++ src/flua/Tools/IDE/Widgets/BPDocNavigator.py
@@ -41,6 +41,39 @@ class BPDocNavigator(QtGui.QWidget):
 					elif x.startswith(nextNumber):
 						self.nextFile = fixPath(fileDir + x)
 		
+		if (not self.nextFile) or (not self.previousFile):
+			category = parts[-2]
+			categoryNum = int(category[:2])
+			
+			prevCat = str(categoryNum - 1)
+			nextCat = str(categoryNum + 1)
+			
+			#if len(prevCat) == 1:
+			#	prevCat = "0" + prevCat
+			
+			#if len(nextCat) == 1:
+			#	nextCat = "0" + nextCat
+			
+			catDir = fixPath("/".join(parts[:-2]))
+			highest = 0
+			
+			for root, subFolders, files in os.walk(catDir):
+				#print("ROOT: " + root)
+				#print(nextCat)
+				
+				if (not self.nextFile) and os.path.basename(root).startswith(nextCat):
+					for x in files:
+						if x.startswith("01"):
+							self.nextFile = fixPath(root + "/" + x)
+							
+				if (not self.previousFile) and os.path.basename(root).startswith(prevCat):
+					for x in files:
+						if len(x) >= 3:
+							num = x[:2]
+							if num.isdigit() and int(num) > highest:
+								self.previousFile = fixPath(root + "/" + x)
+								highest = int(num)
+		
 		# Create the buttons
 		self.group = QtGui.QButtonGroup(self)
 		
