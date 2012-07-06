1239b38598d65146a58921f7c963f7d447625dd9
Eduard Urbach <e.urbach@gmail.com>
2012-07-06 22:16:26 +0200

    Added simple wrapping methods for up to 2 parameters

--- src/flua/Core/Startup.hpp
+++ src/flua/Core/Startup.hpp
@@ -1,2 +1,3 @@
 // Simple wrapper functions
 #define BP_WRAP_1(_TYPE, _BP_FUNC, _C_FUNC) template <typename _T> inline _TYPE _BP_FUNC (_T _param) { return _C_FUNC(_param); }
+#define BP_WRAP_2(_TYPE, _BP_FUNC, _C_FUNC) template <typename _T1, typename _T2> inline _TYPE _BP_FUNC (_T1 _param1, _T2 _param2) { return _C_FUNC(_param1, _param2); }
--- src/flua/Math/Geometry/Geometry.hpp
+++ src/flua/Math/Geometry/Geometry.hpp
@@ -7,7 +7,7 @@ BP_WRAP_1(float, flua_tan, tanf)
 BP_WRAP_1(float, flua_asin, asinf)
 BP_WRAP_1(float, flua_acos, acosf)
 BP_WRAP_1(float, flua_atan, atanf)
-BP_WRAP_1(float, flua_atan2, atan2f)
+BP_WRAP_2(float, flua_atan2, atan2f)
 
 BP_WRAP_1(float, flua_sinh, sinhf)
 BP_WRAP_1(float, flua_cosh, coshf)
