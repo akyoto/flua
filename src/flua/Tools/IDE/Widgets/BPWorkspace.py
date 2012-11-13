44998c6a9ceb7405a4762cab5b214184c41c36c6
Eduard Urbach <e.urbach@gmail.com>
2012-11-13 13:14:01 +0100

    Fixed adding files to the recently closed files list

--- src/flua/Tools/IDE/Widgets/BPWorkspace.py
+++ src/flua/Tools/IDE/Widgets/BPWorkspace.py
@@ -129,8 +129,10 @@ class BPWorkspace(QtGui.QTabWidget):
 		return -1, None
 		
 	def closeCodeEdit(self, index):
-		if self.widget(index) == self.bpIDE.codeEdit and self.bpIDE.codeEdit is not None:
-			path = self.bpIDE.getFilePath()
+		ce = self.widget(index)
+		
+		if ce:
+			path = ce.getFilePath()
 			if path and not self.bpIDE.isTmpPath(path):
 				self.filesClosed.append(path)
 		
